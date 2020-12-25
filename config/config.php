61e2b68fa5cb65d0b6b26a13e5117b205ce014a1
sergotail <sergotail@mail.ru>
2020-12-25 21:08:40 +0300

    Support laravel storage for custom dicts

--- config/config.php
+++ config/config.php
@@ -10,15 +10,28 @@ return [
 
     'morphies' => [
         [
-            // by this name specific morphy can be accessed through Morphy::morphy method
-            // it may be any string
+            // phpMorphy instance name
+            // specific morphy can be accessed through Morphy::morphy($name) method
             'name' => SEOService2020\Morphy\Morphy::russianLang,
+
+            // phpMorphy language
+            // see Morphy class for available values
             'language' => SEOService2020\Morphy\Morphy::russianLang,
-            // if no options key or null value specified, default options will be used
+
+            // phpMorphy options
+            // if not specified or null specified, default options will be used
             // to use common options from this config, specify []
             'options' => [],
-            // when null specified, default morphy dicts path will be used
+            
+            // dicts directory path
+            // if not specified or null specified, default dicts path will be used
             'dicts_path' => null,
+
+            // values are 'storage', 'filesystem'
+            // 'storage': dicts will be taken from laravel storage (storage/app folder)
+            // 'filesystem': dicts will be taken by specified path as-is
+            // must be specified when dicts_path is not null
+            'dicts_storage' => 'storage',
         ],
     ],
 ];
--- src/Factory.php
+++ src/Factory.php
@@ -2,18 +2,43 @@
 
 namespace SEOService2020\Morphy;
 
+use InvalidArgumentException;
+
+use Illuminate\Support\Facades\Storage;
+
 
 class Factory
 {
     public static function makeMorphy(array $config, ?array $commonOptions = null): Morphy
     {
-        return new Morphy(
-            $config['language'],
-            ($config['options'] ?? null) === null ?
-                null :
-                array_replace_recursive($commonOptions ?? [], $config['options'] ?? []),
-            $config['dicts_path'] ?? null
-        );
+        if (!array_key_exists('language', $config)) {
+            throw new InvalidArgumentException(
+                "language must be specified for morphy {$config['name']}"
+            );
+        }
+
+        if (!array_key_exists('dicts_storage', $config)) {
+            throw new InvalidArgumentException(
+                "dicts_storage must be specified for morphy {$config['name']}"
+            );
+        }
+
+        $dictsStorage = $config['dicts_storage'];
+        if (!in_array($dictsStorage, ['storage', 'filesystem'])) {
+            throw new InvalidArgumentException(
+                "Wrong dicts_storage specified for morphy {$config['name']}: $dictsStorage"
+            );
+        }
+
+        $options = $config['options'] ?? null;
+        $options = $options === null ?:
+            array_replace_recursive($commonOptions ?? [], $options);
+
+        $dictsPath = $config['dicts_path'] ?? null;
+        $dictsPath = ($dictsPath === null || $dictsStorage !== 'storage') ?:
+            Storage::path($dictsPath);
+
+        return new Morphy($config['language'], $options, $dictsPath);
     }
 
     public static function fromArray(array $configs, ?array $commonOptions = null): array
